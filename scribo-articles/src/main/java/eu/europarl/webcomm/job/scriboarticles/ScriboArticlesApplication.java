078dfff4db5675bcad1a6c8f407d0e115e6e0fe4
Victor Valentin Sindie <victor.sindie@ext.europarl.europa.eu>
2017-09-14 17:11:51 +0200

    Initial commit

---
+++ scribo-articles/src/main/java/eu/europarl/webcomm/job/scriboarticles/ScriboArticlesApplication.java
@@ -0,0 +1,12 @@
+package eu.europarl.webcomm.job.scriboarticles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriboArticlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriboArticlesApplication.class, args);
+	}
+}
---
+++ scribo-articles/src/test/java/eu/europarl/webcomm/job/scriboarticles/ScriboArticlesApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.europarl.webcomm.job.scriboarticles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScriboArticlesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
